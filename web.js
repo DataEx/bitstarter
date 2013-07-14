0e8a54429b452304202601d642a94c117a71a01e
Seth Mishan <smishan1@binghamton.edu>
2013-07-14 01:13:50 +0000

    updated

--- web.js
+++ web.js
@@ -3,12 +3,12 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var str = fs.readFileSync(index.html);
-var buffedStr = str.toString("utf-8",0,26);
+var buffedStr = str.toString("utf-8",0,20);
 
 
 app.get('/', function(request, response) {
   response.send(buffedStr);
-}
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
