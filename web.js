fbe736ae5ef0081497b992f3b3d54193362d2608
Seth Mishan <smishan1@binghamton.edu>
2013-07-14 01:42:58 +0000

    updated

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 var fs = require('fs');
 var strg = fs.readFileSync("index.html");
-var buffedStrg = strg.toString("utf-8",0,20);
+var buffedStrg = strg.toString();
 
 
 app.get('/', function(request, response) {
